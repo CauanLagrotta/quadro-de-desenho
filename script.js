cd174da2a4ffeed008811e372b237757b6389d9a
CauanLagrotta <caun06lagrotta@gmail.com>
2024-09-30 19:57:34 -0300

    updates

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ let canDraw = false;
 let mouseX = 0;
 let mouseY = 0;
 
-
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
 
-
+// Flag to differentiate between touch and mouse events
+let isTouch = false;
 
 //Events
 document.querySelectorAll('.color').forEach((color) => {
-    color.addEventListener('click', (clickColorEvent));
-})
+    color.addEventListener('click', clickColorEvent);
+});
 
 document.querySelector('.clear').addEventListener('click', (e) => {
     ctx.clearRect(0, 0, screen.width, screen.height);
-})
+});
 
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
 screen.addEventListener('mouseup', mouseUpEvent);
 
+// Mobile events
+screen.addEventListener('touchstart', touchStartEvent);
+screen.addEventListener('touchmove', touchMoveEvent);
+screen.addEventListener('touchend', touchEndEvent);
 
-//Functions
+// Global event to stop when leaving canvas
+document.addEventListener('mouseup', mouseUpEvent);
+
+// Functions
 function clickColorEvent(e) {
     let color = e.target.getAttribute('data-color');
     currentColor = color;
@@ -34,27 +41,26 @@ function clickColorEvent(e) {
 }
 
 function mouseDownEvent(e) {
+    if (isTouch) return; 
     canDraw = true;
     mouseX = e.pageX - screen.offsetLeft;
     mouseY = e.pageY - screen.offsetTop;
 }
 
 function mouseMoveEvent(e) {
-    if(canDraw) {
+    if (canDraw && !isTouch) { 
         draw(e.pageX, e.pageY);
-
     }
 }
 
 function mouseUpEvent() {
     canDraw = false;
-    
 }
 
 function draw(x, y) {
     let pointX = x - screen.offsetLeft;
     let pointY = y - screen.offsetTop;
-    
+
     ctx.beginPath();
     ctx.lineWidth = 5;
     ctx.lineJoin = 'round';
@@ -62,13 +68,29 @@ function draw(x, y) {
     ctx.moveTo(mouseX, mouseY);
     ctx.lineTo(pointX, pointY);
     ctx.closePath();
-    ctx.strokeStyle = currentColor;
     ctx.stroke();
 
     mouseX = pointX;
     mouseY = pointY;
 }
 
-function clearScreen() {
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
+// Touch events
+function touchStartEvent(e) {
+    isTouch = true;  
+    canDraw = true;
+    e.preventDefault(); 
+    mouseX = e.touches[0].pageX - screen.offsetLeft;
+    mouseY = e.touches[0].pageY - screen.offsetTop;
+}
+
+function touchMoveEvent(e) {
+    if (canDraw) {
+        e.preventDefault(); 
+        draw(e.touches[0].pageX, e.touches[0].pageY);
+    }
+}
+
+function touchEndEvent() {
+    canDraw = false;
+    isTouch = false;  
 }
